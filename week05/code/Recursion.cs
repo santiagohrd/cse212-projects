eaac678d79a8faa02d0768eada995cc106da8806
SantiagoHrd <santiagohrd1@gmail.com>
2025-08-02 06:15:40 -0500

    w5 Code

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -15,7 +15,9 @@ public static class Recursion
     public static int SumSquaresRecursive(int n)
     {
         // TODO Start Problem 1
-        return 0;
+        if (n <= 0)
+        return 0; 
+        return n * n + SumSquaresRecursive(n - 1);
     }
 
     /// <summary>
@@ -40,6 +42,17 @@ public static class Recursion
     public static void PermutationsChoose(List<string> results, string letters, int size, string word = "")
     {
         // TODO Start Problem 2
+        if (word.Length == size)
+        {
+            results.Add(word);
+            return;
+        }
+
+        for (int i = 0; i < letters.Length; i++)
+        {
+            string remaining = letters.Remove(i, 1);
+            PermutationsChoose(results, remaining, size, word + letters[i]);
+        }
     }
 
     /// <summary>
@@ -97,9 +110,22 @@ public static class Recursion
             return 4;
 
         // TODO Start Problem 3
+        if (remember == null)
+    {
+        remember = new Dictionary<int, decimal>();
+    }
 
+    // Base cases
+        if (s < 0) return 0;
+        if (s == 0) return 1; 
+        if (remember.ContainsKey(s))
+        {
+            return remember[s];
+        }   
         // Solve using recursion
-        decimal ways = CountWaysToClimb(s - 1) + CountWaysToClimb(s - 2) + CountWaysToClimb(s - 3);
+        decimal ways = CountWaysToClimb(s - 1, remember) + CountWaysToClimb(s - 2, remember) + CountWaysToClimb(s - 3, remember);
+        remember[s] = ways; 
+
         return ways;
     }
 
@@ -119,6 +145,16 @@ public static class Recursion
     public static void WildcardBinary(string pattern, List<string> results)
     {
         // TODO Start Problem 4
+        int index = pattern.IndexOf('*');
+
+        if (index == -1)
+        {
+            results.Add(pattern);
+            return;
+        }
+
+        WildcardBinary(pattern[..index] + '0' + pattern[(index + 1)..], results);
+        WildcardBinary(pattern[..index] + '1' + pattern[(index + 1)..], results);
     }
 
     /// <summary>
@@ -137,7 +173,7 @@ public static class Recursion
 
         // TODO Start Problem 5
         // ADD CODE HERE
-
+        //I couldn't figure it out how to solve this
         // results.Add(currPath.AsString()); // Use this to add your path to the results array keeping track of complete maze solutions when you find the solution.
     }
 }
